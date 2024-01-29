a55026ef2de5bab0c3c5b881346e8327f0a018e2
Diwakar Gupta <39624018+Diwakar-Gupta@users.noreply.github.com>
2024-01-29 20:13:19 +0530

    init

---
+++ src/src/App.jsx
@@ -0,0 +1,35 @@
+import React, { lazy, Suspense } from "react";
+import { createBrowserRouter, RouterProvider, Outlet } from "react-router-dom";
+import Progress from "./components/Progress";
+
+const Error = lazy(() => import("./pages/Error"));
+
+const AppLayout = () => {
+  console.log("AppLayout Rendered");
+  return (
+    <div className="app">
+      <Outlet />
+    </div>
+  );
+};
+
+const appRouter = createBrowserRouter([
+  {
+    path: "/",
+    element: <AppLayout />,
+    errorElement: <Error />,
+    children: [
+      {
+        path: "/",
+        element: (
+          <Suspense fallback={<Progress />}>
+            <h1 class="text-3xl font-bold underline">Hello world!</h1>
+          </Suspense>
+        ),
+      },
+    ],
+  },
+]);
+const AppRouter = () => <RouterProvider router={appRouter} />;
+
+export default AppRouter;
